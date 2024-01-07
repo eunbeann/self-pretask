8027a03100b5dbab89161d00d9c532ab06d688e1
eunbeann <ds5bean23@gmail.com>
2024-01-08 00:05:37 +0900

    feat: router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
-import StickerPicker from "./components/StickerPicker";
 import NotFound from "./pages/NotFound";
+import Example from "./pages/Example";
+import Origin from "./pages/Origin";
+import StickerPack from "./pages/StickerPack";
+import Attached from "./pages/Attached";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/sticker-picker" element={<StickerPicker />} />
+        <Route path="/sticker-picker" element={<StickerPack />} />
+        <Route path="/attached" element={<Attached />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/example" element={<Example />} />
+        <Route path="/original" element={<Origin />} />
       </Routes>
     </BrowserRouter>
   );
